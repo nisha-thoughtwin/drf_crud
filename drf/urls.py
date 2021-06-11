937398a2e4f82d59951a165a8eaa1a7e67b03650
nisha <nisha.thoughtwin@gmail.com>
2021-06-11 16:51:24 +0530

    .

--- drf/urls.py
+++ drf/urls.py
@@ -14,26 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
-from employee.auth import CustomAuthToken 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from employee.auth import CustomAuthToken
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 def trigger_error(request):
     division_by_zero = 1 / 0
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('employee/',include('employee.urls')),
-    path('gettoken/',obtain_auth_token),
-    path('custom_gettoken/',CustomAuthToken.as_view()),
 
-    path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+urlpatterns = [
+    path('', admin.site.urls),
+    path('employee/', include('employee.urls')),
+    path('gettoken/', obtain_auth_token),
+    path('custom_gettoken/', CustomAuthToken.as_view()),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refreshtoken/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('verifytoken/', TokenVerifyView.as_view(), name='token_verify'),
 
     path('sentry-debug/', trigger_error),
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
